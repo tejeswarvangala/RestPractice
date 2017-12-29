689a1149674f5f1b4073d644161923575f5ed93b
MrinaliniDonthireddy <mdonthireddy@hotmail.com>
2017-12-28 21:01:41 -0600

    changes in pom.xml and Customer class

--- src/main/java/com/spring/customer/model/Customer.java
+++ src/main/java/com/spring/customer/model/Customer.java
@@ -3,27 +3,36 @@ package com.spring.customer.model;
 public class Customer {
 	private String firstName;
 	private String lastName;
+
 	public Customer(String firstName, String lastName) {
-		this.firstName=firstName;
-		this.lastName=lastName;
-		
+		this.firstName = firstName;
+		this.lastName = lastName;
+
 	}
-	public Customer(){
-		
+
+	public Customer() {
+
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
-	
 
 }
